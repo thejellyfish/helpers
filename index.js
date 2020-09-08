46bc7d7909ee68052e3fef27c7ce51c26a809424
Mohamed B <mohamed@jellyfish-commuting.com>
2020-09-08 16:33:14 +0200

    Rename metric helpers in plural

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const _capitalize = require('./_capitalize');
 const _get = require('./_get');
-const _metric = require('./_metric');
+const _metrics = require('./_metrics');
 const _queryString = require('./_queryString');
 const _set = require('./_set');
 const _ucfirst = require('./_ucfirst');
@@ -8,7 +8,7 @@ const _ucfirst = require('./_ucfirst');
 module.exports = {
   _capitalize,
   _get,
-  _metric,
+  _metrics,
   _queryString,
   _set,
   _ucfirst,
