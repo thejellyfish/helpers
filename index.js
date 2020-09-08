c1520e29965da8238a89f4ac888ef5c4c8ee91a7
Mohamed B <mohamed@jellyfish-commuting.com>
2020-09-08 16:29:57 +0200

    Add metric helper skeleton

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const _capitalize = require('./_capitalize');
 const _get = require('./_get');
+const _metric = require('./_metric');
 const _queryString = require('./_queryString');
 const _set = require('./_set');
 const _ucfirst = require('./_ucfirst');
@@ -7,6 +8,7 @@ const _ucfirst = require('./_ucfirst');
 module.exports = {
   _capitalize,
   _get,
+  _metric,
   _queryString,
   _set,
   _ucfirst,
