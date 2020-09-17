c51c0c143a86685a476a57cea85e0f2a8be1ded9
Mohamed B <mohamed@jellyfish-commuting.com>
2020-09-17 14:17:01 +0200

    Minor updates _queryString

--- _queryString.js
+++ _queryString.js
@@ -4,9 +4,9 @@ const _reduce = require('./_reduce');
 // queryString
 //-------
 module.exports = function (data) {
-  return _reduce(data, (acc, item, key) => (
+  return _reduce(data, (acc, item, key) => acc.concat(
     Array.isArray(item)
-      ? acc.concat(item.map(elem => `${encodeURIComponent(key)}[]=${encodeURIComponent(elem)}`).join('&'))
-      : acc.concat(`${encodeURIComponent(key)}=${encodeURIComponent(item)}`)
+      ? item.map(elem => `${encodeURIComponent(key)}[]=${encodeURIComponent(elem)}`).join('&')
+      : `${encodeURIComponent(key)}=${encodeURIComponent(item)}`
   ), []).join('&');
 };
