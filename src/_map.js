a100223267c58650dcd30ea953aa47f25f1a3cf1
thejellyfish <the@jellyfish-commuting.com>
2023-12-15 23:10:02 +0100

    Rollback _map helper

--- src/_map.js
+++ src/_map.js
@@ -2,11 +2,5 @@
 // Creates a new object populated with the results of iteratee
 //-------
 module.exports = function (object, iteratee) {
-  const result = {};
-
-  Object.keys(object).forEach(key => {
-    result[key] = iteratee(object[key], key, object);
-  });
-
-  return result;
+  return Object.keys(object).map(key => iteratee(object[key], key, object));
 };
